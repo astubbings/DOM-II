8634264a179942b8a007d64d848b15717a119bee
astubbings <53932400+astubbings@users.noreply.github.com>
2019-10-29 14:43:51 -0600

    commit for nav animation

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ funBusTextChange.addEventListener("mouseenter", () => {
     funBusTextChange.style.border ='2px solid black';
     funBusTextChange.style.padding = '0 1%';
     funBusTextChange.style.color = "darkorange";
+    funBusTextChange.textContent = "Funnner Busss";
 })
 
 funBusTextChange.addEventListener("mouseleave", () => {
@@ -15,4 +16,22 @@ funBusTextChange.addEventListener("mouseleave", () => {
     funBusTextChange.style.border ='2px solid yellow';
     funBusTextChange.style.padding = '1% 1% 0 1%';
     funBusTextChange.style.color = "black";
+})
+
+const funBusNavTextChange = document.querySelectorAll('.nav a').forEach(eachNavA => {
+    eachNavA.addEventListener("mouseover", () => {
+    eachNavA.style.transform = 'scale(1.2)';
+    eachNavA.style.transition = 'all 50ms';
+    eachNavA.style.padding = '1px';
+    eachNavA.style.border = '1px dotted firebrick';
+    })
+
+})
+const funBusNavTextChangeRemove = document.querySelectorAll('.nav a').forEach(eachNavA => {
+    eachNavA.addEventListener("mouseleave", () => {
+    eachNavA.style.transform = 'scale(1.0)';
+    eachNavA.style.transition = 'all 2000ms';
+    eachNavA.style.padding = '1px';
+    eachNavA.style.border = '1px dotted white';
+    })
 })
